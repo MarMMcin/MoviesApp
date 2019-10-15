05c68286d8507e732ca380272b5fefd43339466f
MarMMcin <em-erce@wp.pl>
2019-10-15 13:32:32 +0200

    Updates

---
+++ precache-manifest.d07582ca9efa4d70fa8171a36ec02ac1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90d63554967ee21ebaf9",
+    "url": "/MarMMcin/MoviesApp/static/js/runtime~main.90d63554.js"
+  },
+  {
+    "revision": "c24cddde9dd2ebe5cfa8",
+    "url": "/MarMMcin/MoviesApp/static/js/main.c24cddde.chunk.js"
+  },
+  {
+    "revision": "aaa51877e6dc757d0dee",
+    "url": "/MarMMcin/MoviesApp/static/js/1.aaa51877.chunk.js"
+  },
+  {
+    "revision": "c24cddde9dd2ebe5cfa8",
+    "url": "/MarMMcin/MoviesApp/static/css/main.af844e2c.chunk.css"
+  },
+  {
+    "revision": "72fcf5d8ab8ffa57b30e652ad464be6e",
+    "url": "/MarMMcin/MoviesApp/index.html"
+  }
+];
